e5e69ebf42b1c797ed08d8b3ca29c0ac3f45cc74
hajin <74534952+hajin0324@users.noreply.github.com>
2024-02-27 23:48:42 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.21 ms
+메모리: 10.3 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 27일 23:48:09
+2024년 02월 27일 23:48:38
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,11 +1,11 @@
 def solution(numer1, denom1, numer2, denom2):
-    numer = numer1 * denom2 + numer2 * denom1
-    denom = denom1 * denom2
+    num = numer1 * denom2 + numer2 * denom1
+    den = denom1 * denom2
     gcd = 0
     
-    for i in range(min(numer, denom), 0, -1):
-        if numer % i == 0 and denom % i == 0:
+    for i in range(min(num, den), 0, -1):
+        if num % i == 0 and den % i == 0:
             gcd = i
             break
     
-    return [numer / gcd, denom / gcd]
+    return [num / gcd, den / gcd]
