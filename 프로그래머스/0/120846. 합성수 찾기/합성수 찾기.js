8598f0e754ba884e3a417245633ef076c3310b1d
hajin <74534952+hajin0324@users.noreply.github.com>
2024-03-08 22:52:36 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let cnt = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                cnt++;
+                break;
+            }
+        }
+    }
+    
+    return cnt;
+}
