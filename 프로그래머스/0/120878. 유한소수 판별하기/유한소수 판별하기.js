6c3a717642562cb74cfd6482e5a510238ec0842b
hajin <74534952+hajin0324@users.noreply.github.com>
2024-03-15 16:56:00 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,16 @@
+function solution(a, b) {
+    let gcd = 0;
+    
+    for (let i = a; i >= 1; i--) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i
+            break
+        }
+    }
+    
+    b /= gcd
+    while (b % 2 == 0) b /= 2
+    while (b % 5 == 0) b /= 5
+    
+    return b == 1 ? 1 : 2
+}
