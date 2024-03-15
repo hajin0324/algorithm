d6f2183ff4df70bf90a837e71bce0fad7fca8dd9
hajin <74534952+hajin0324@users.noreply.github.com>
2024-03-15 17:21:04 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/README.md
+++ 프로그래머스/0/120878. 유한소수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 유한소수 판별하기 - 120878 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 33.4 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 17:04:55
+2024년 03월 15일 17:21:02
 
 ### 문제 설명
 
--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -3,14 +3,14 @@ function solution(a, b) {
     
     for (let i = a; i >= 1; i--) {
         if (a % i == 0 && b % i == 0) {
-            gcd = i
-            break
+            gcd = i;
+            break;
         }
     }
     
-    b /= gcd
-    while (b % 2 == 0) b /= 2
-    while (b % 5 == 0) b /= 5
+    b /= gcd;
+    while (b % 2 == 0) b /= 2;
+    while (b % 5 == 0) b /= 5;
     
-    return b == 1 ? 1 : 2
+    return b == 1 ? 1 : 2;
 }
