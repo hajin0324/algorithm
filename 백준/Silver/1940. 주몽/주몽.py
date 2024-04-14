662f8380de0edccd2172d359f86c7d634a9a2116
hajin <74534952+hajin0324@users.noreply.github.com>
2024-04-14 23:01:33 +0900

    [Silver IV] Title: 주몽, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+
+s = 0
+e = n - 1
+cnt = 0
+
+while s < e:
+  if arr[s] + arr[e] > m:
+    e -= 1
+  elif arr[s] + arr[e]  < m:
+    s += 1
+  else:
+    cnt += 1
+    s += 1
+    e -= 1
+
+print(cnt)
