560094b00893b4a57378cfb6e22757898059c57a
hajin <74534952+hajin0324@users.noreply.github.com>
2024-11-01 16:39:49 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.61 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -2,12 +2,12 @@ function solution(s) {
     let ans = [0, 0];
     
     while (s != 1) {
-        let len = s.length
+        let len = s.length;
         ans[0] += 1;
         s = [...s].filter(e => e != 0);
-        ans[1] += len - s.length
-        s = s.length.toString(2)
+        ans[1] += len - s.length;
+        s = s.length.toString(2);
     }
     
-    return ans
+    return ans;
 }
