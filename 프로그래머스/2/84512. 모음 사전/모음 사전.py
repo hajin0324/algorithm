7edef51fa692a7e50547619a34b966df841d8eae
hajin <74534952+hajin0324@users.noreply.github.com>
2024-12-17 15:08:18 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -3,24 +3,9 @@ from collections import deque
 def solution(word):
     ans = 0
     alp = ['A', 'E', 'I', 'O', 'U']
-    q = deque([['A']])
     
-    while q:
-        w = q.popleft()
-        ans += 1
-        if ''.join(w) == word:
-            break
-            
-        if len(w) != 5:
-            w.append('A')
-        else:
-            if w[-1] == 'U':
-                while w[-1] == 'U':
-                    w.pop()
-
-            idx = alp.index(w[-1]) 
-            w[-1] = alp[idx + 1]
-            
-        q.append(w)
-        
+    for i, w in enumerate(word):
+        weight = sum(5 ** j for j in range(4 - i, -1, -1))
+        ans += weight * alp.index(w) + 1
+    
     return ans
