113828e0670513179c68e6210ac8bc93d2bb15a0
hajin <74534952+hajin0324@users.noreply.github.com>
2025-02-12 11:52:55 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 2628 ms, Memory: 136908 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -1,18 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-def move_cost(s, e):
-  if s == e:
-    return 1
-  elif s == 0:
-    return 2
-  elif (s - e + 4) % 2 != 0:
-    return 3
-  else:
-    return 4
-
 A = list(map(int, input().split()))
 N = len(A)
+M = [[0, 2, 2, 2, 2], 
+     [2, 1, 3, 4, 3], 
+     [2, 3, 1, 3, 4], 
+     [2, 4, 3, 1, 3], 
+     [2, 3, 4, 3, 1]]
+
 D = [[[sys.maxsize for k in range(5)] for j in range(5)] for i in range(100001)]
 D[0][0][0] = 0
 
@@ -23,17 +19,13 @@ for i in range(1, N):
     if j == p:
       continue
     for k in range(5):
-      D[i][j][p] = min(D[i - 1][j][k] + move_cost(k, p), D[i][j][p])
+      D[i][j][p] = min(D[i - 1][j][k] + M[k][p], D[i][j][p])
 
   for j in range(5):
     if j == p:
       continue
     for k in range(5):
-      D[i][p][j] = min(D[i - 1][k][j] + move_cost(k, p), D[i][p][j])
+      D[i][p][j] = min(D[i - 1][k][j] + M[k][p], D[i][p][j])
 
-result = sys.maxsize
-for i in range(5):
-  for j in range(5):
-    result = min(result, D[N - 1][i][j])
-
+result = min(D[N - 1][i][j] for i in range(5) for j in range(5))
 print(result)
