ca7fb9cfe9a647148edf9d1c66ece6598671884c
hajin <74534952+hajin0324@users.noreply.github.com>
2025-02-12 12:14:14 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 956 ms, Memory: 33976 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -2,30 +2,32 @@ import sys
 input = sys.stdin.readline
 
 A = list(map(int, input().split()))
-N = len(A)
-M = [[0, 2, 2, 2, 2], 
+A.pop()
+
+C = [[0, 2, 2, 2, 2], 
      [2, 1, 3, 4, 3], 
      [2, 3, 1, 3, 4], 
      [2, 4, 3, 1, 3], 
      [2, 3, 4, 3, 1]]
 
-D = [[[sys.maxsize for k in range(5)] for j in range(5)] for i in range(100001)]
-D[0][0][0] = 0
+D = [[sys.maxsize] * 5 for _ in range(5)]
+D[0][0] = 0
 
-for i in range(1, N):
-  p = A[i - 1]
+for a in A:
+  N = [[sys.maxsize] * 5 for _ in range(5)]
 
-  for j in range(5):
-    if j == p:
-      continue
-    for k in range(5):
-      D[i][j][p] = min(D[i - 1][j][k] + M[k][p], D[i][j][p])
+  for i in range(5):
+    for j in range(5):
+      if D[i][j] == sys.maxsize:
+        continue
 
-  for j in range(5):
-    if j == p:
-      continue
-    for k in range(5):
-      D[i][p][j] = min(D[i - 1][k][j] + M[k][p], D[i][p][j])
+      if i == a or j == a:
+        N[i][j] = min(N[i][j], D[i][j] + 1)
 
-result = min(D[N - 1][i][j] for i in range(5) for j in range(5))
-print(result)
+      else: 
+        N[a][j] = min(N[a][j], D[i][j] + C[i][a])
+        N[i][a] = min(N[i][a], D[i][j] + C[j][a])
+
+  D = N
+
+print(min(min(row) for row in D))
